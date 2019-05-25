dcd0f015c4ff01b6ef0d10b2b027eac8542b4ac9
Marcos Lima <marcoslimadefatima@gmail.com>
2019-05-25 15:01:21 -0300

    So vai

--- CI-CD.Core/Entities/Product.cs
+++ CI-CD.Core/Entities/Product.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace CI_CD.Core.Entities
 {
-    class Product
+    public class Product
     {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string Description { get; set; }
+        public double Value { get; private set; }
+
     }
 }
